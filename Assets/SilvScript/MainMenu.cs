25f96b825ea41cd39469a8b7043e05705b002c8c
SRoseCrystal <silvanarosecrystal@gmail.com>
2022-10-24 12:39:14 +0100

    Started on the actual menu buttons

--- Assets/SilvScript/MainMenu.cs
+++ Assets/SilvScript/MainMenu.cs
@@ -6,16 +6,28 @@ public class MainMenu : MonoBehaviour
 {
 
     public bool isStart;
+    public bool isSettings;
     public bool isQuit;
+
+    public bool isBack;
     void OnMouseUp()
     {
         if (isStart)
         {
             Application.LoadLevel(1);
         }
+        if (isSettings)
+        {
+            Application.LoadLevel(2);
+        }
         if (isQuit)
         {
             Application.Quit();
+        }
+
+        if (isBack)
+        {
+            Application.LoadLevel(0);
         }
     }
 }
